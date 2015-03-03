851f6445f42ad94e1f6c44d7a1e2af3e3103599c
Vaughany <paulieboo@gmail.com>
2015-03-03 17:24:47 +0000

    Start of moving more admin functions into seperate files.

---
+++ functions-admin-general.inc.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * General functions.
+ */
+
+// If there are any 'alerts' set, do cool stuff.
+function display_alerts() {
+
+  if ( isset( $_SESSION['alerts'] ) ) {
+
+    $out = '';
+    foreach ( $_SESSION['alerts'] as $alert ) {
+
+      foreach ( $alert as $alert_type => $alert_text ) {
+
+        // Catch 'danger' alerts and ensure they can't be dismissed.
+        if ( $alert_type == 'danger' ) {
+          $out .= '<div class="alert alert-' . $alert_type . '" role="alert">' . "\n";
+
+        } else {
+          $out .= '<div class="alert alert-' . $alert_type . ' alert-dismissible alert-' . $alert_type . '-fade" role="alert">' . "\n";
+          $out .= '  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' . "\n";
+        }
+
+        $out .= $alert_text . "\n";
+        $out .= '</div>' . "\n";
+
+      }
+
+    }
+
+    unset( $_SESSION['alerts'] );
+    return $out;
+  }
+
+}
