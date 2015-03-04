ca9e2b4871ba5d07cf2ebbaf3a1cd27de4135548
Vaughany <paulieboo@gmail.com>
2015-03-04 17:51:03 +0000

    Saner default config settings.

--- config-dist.inc.php
+++ config-dist.inc.php
@@ -3,11 +3,13 @@
 /**
  * Default configuration options.
  * Add in your details and save as config.inc.php
+ *
+ * Note: It is not good practice to log in as 'root'! Instead, create a user with limited permissions.
  */
 
-$CFG['db']['name']      = 'name';       // e.g. 'thescreen'
-$CFG['db']['host']      = 'host';       // e.g. 'localhost'
-$CFG['db']['user']      = 'user';       // e.g. 'screenuser'
-$CFG['db']['pwd']       = 'password';   // e.g. '4v3Ry$tr0NgP@Ssw0rD'
+$CFG['db']['name']      = 'thescreen';  // e.g. 'thescreen'
+$CFG['db']['host']      = 'localhost';  // e.g. 'localhost' or an IP address
+$CFG['db']['user']      = '';           // e.g. 'screenuser'
+$CFG['db']['pwd']       = '';           // e.g. '4v3Ry$tr0NgP@Ssw0rD'
 
 $CFG['login']['pwd']    = 'a1b2c3...';  // 64 digit SHA-256 hash of a strong password
