3de5486e11ecf4a275cd4883dc426c123a2b1ffa
Vaughany <paulieboo@gmail.com>
2015-03-04 17:48:58 +0000

    Moved the defaults to a seperate file so we can use them elsewhere, e.g. installer.

---
+++ defaults.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Initial configuration settings array.
+ */
+
+$CFG['defaults'] = array(
+  'page'          => 1,
+  'status'        => 1,
+  'refresh'       => 300, // May not need this one much longer...
+  'rssfeed'       => 'http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/technology/rss.xml',
+  'showstopper'   => 'error!',
+  'specific_fig'  => 'aaa-random.png',
+  'changes'       => 'no',
+);
