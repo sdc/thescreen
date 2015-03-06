b37424a385166d1d5bc6e645b57710039668ee08
Paul Vaughan <paulieboo@gmail.com>
2015-03-06 14:51:52 +0000

    Added a note about future changes.

--- refresh.php
+++ refresh.php
@@ -2,6 +2,9 @@
 
 /**
  * 'Refresh' web service to return the 'changes' config setting. If it is yes, change it back to no.
+ *
+ * TODO: The 'yes' or 'no' booleans may have to change in favour of a 'last updated' timestamp or Unix epoch setting. 
+ *       This will enable more than one screen to talk back to the server and refresh if changes have been made.
  */
 
 require_once( 'functions.inc.php' );
