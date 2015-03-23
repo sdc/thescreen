ade2cc5eac78f1e2696345c402556856d712faf9
Paul Vaughan <paulieboo@gmail.com>
2015-03-23 14:01:42 +0000

    version.php

---
+++ version.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Version information.
+ */
+
+// yyyymmdd
+$CFG['version']['date']   = '20150323';
+
+// http://semver.org/ - major.minor.patch
+$CFG['version']['build']  = '0.2.3';
